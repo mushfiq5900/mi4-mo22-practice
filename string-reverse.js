9f66b7258f53aa98d387d7d5ef8156a9b9b81d25
Mushfiq <mushfiquli@gmail.com>
2022-04-09 11:04:38 +0600

    Full porject Upload

---
+++ string-reverse.js
@@ -0,0 +1,14 @@
+// function reverse(inpString) {
+//     console.log(inpString);
+//     let rev = '';
+//     for (var letter of inpString) {
+//         rev = letter + rev;
+//     }
+//     console.log(rev);
+// }
+// var inString = 'amar nam abid';
+// var output = reverse(inString);
+
+var student = { name: "Yo Mama", age: 17 };
+
+console.log(student);
